812da9526ee5b7c55a130578b2f35ad8c105ea3d
セコイア <sekoia2900@gmail.com>
2026-01-15 11:19:59 +0900

    Update graph_editor_widget.py

--- modules/gui/graph_editor_widget.py
+++ modules/gui/graph_editor_widget.py
@@ -1,3 +1,5 @@
+#graph_editor_widget.py
+
 from PySide6.QtWidgets import QWidget
 from PySide6.QtCore import Qt, Signal, Slot, QSize, QRect, QPoint
 from PySide6.QtGui import QPainter, QColor, QBrush, QPen, QPaintEvent, QMouseEvent
