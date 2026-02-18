c0905d9bdb48c42c41307d0b54366e16063628ea
セコイア <sekoia2900@gmail.com>
2026-02-18 19:37:12 +0900

    SE18

--- modules/gui/main_window.py
+++ modules/gui/main_window.py
@@ -14,6 +14,7 @@ import zipfile
 import shutil
 import threading
 import math
+from copy import deepcopy
 from typing import Any, List, Dict, Optional, TYPE_CHECKING, cast              
 
 # ==========================================================================
