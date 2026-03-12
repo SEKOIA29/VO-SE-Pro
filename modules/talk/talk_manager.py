502676944a6af24270b66d51beb11e529e145f15
セコイア <sekoia2900@gmail.com>
2026-03-12 17:22:05 +0900

    Update talk_manager.py

--- modules/talk/talk_manager.py
+++ modules/talk/talk_manager.py
@@ -462,6 +462,7 @@ class TalkManager(QObject):
         finally:
             self.is_speaking = False
             self.speak_finished.emit()
+            tmp_path: str | None = None 
             # 一時ファイルを削除
             try:
                 if "tmp_path" in locals() and os.path.exists(tmp_path):
