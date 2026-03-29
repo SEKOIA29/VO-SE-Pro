dff25d113e8be9a977f27cb44b2ca05e9d3b5e0b
セコイア <sekoia2900@gmail.com>
2026-03-29 13:37:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def get_resource_path(relative_path):
     if getattr(sys, 'frozen', False):
         base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     else:
-        base_path = os.path.abspath(".")
+        base_path = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(base_path, relative_path)
 
 # --- [2] 設定管理クラス (ConfigHandler) ---
