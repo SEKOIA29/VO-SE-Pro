0309f56bd963b86554d33154036909678ead7a42
セコイア <sekoia2900@gmail.com>
2026-03-30 19:05:24 +0900

    Reorder items in __all__ for consistency

--- modules/ffi/__init__.py
+++ modules/ffi/__init__.py
@@ -1,3 +1,3 @@
-from .vose_types import CNoteEvent, as_c_double_array
+from .vose_types import CNoteEvent, as_c_double_array, validate_note_event_layout
 
-__all__ = ["CNoteEvent", "validate_note_event_layout", "as_c_double_array"]
+__all__ = ["CNoteEvent", "as_c_double_array", "validate_note_event_layout"]
